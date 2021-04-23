2cc96e2a23f33d394468631c897582f17593d5bb
Sami <sami.seisalo@gmail.com>
2021-04-23 12:00:05 +0300

    unicafe - 6.1

--- osa6/unicafe-redux/src/reducer.js
+++ osa6/unicafe-redux/src/reducer.js
@@ -8,13 +8,17 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return {...initialState, good: initialState.good + 1}
     case 'OK':
-      return state
+      return {...initialState, ok: initialState.ok + 1}
     case 'BAD':
-      return state
+      return {...initialState, bad: initialState.bad + 1}
     case 'ZERO':
-      return state
+      return {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
     default: return state
   }
   
